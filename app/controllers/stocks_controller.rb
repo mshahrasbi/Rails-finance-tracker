4e0f9405d331883c61a230921a19bb99a1ba6b72
mshahrasbi <mshahrasbi@msn.com>
2019-12-09 15:03:49 -0700

    invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,10 +2,19 @@
 class StocksController < ApplicationController
 
     def search 
-        @stock = Stock.new_from_lookup(params[:stock])
-
-        # render json: @stock
-        render 'users/my_portfolio'
+        if params[:stock].present?
+            @stock = Stock.new_from_lookup(params[:stock])
+            if @stock
+                # render json: @stock
+                render 'users/my_portfolio'
+            else
+                flash[:danger] = "You have entered an incorrect symbol"
+                redirect_to my_protfolio_path
+            end
+        else
+            flash[:danger] = "You have entered an empty search string"
+            redirect_to my_protfolio_path            
+        end
     end
 
 end
