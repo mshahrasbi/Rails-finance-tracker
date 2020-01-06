e0b451cd83afdebc1a453fa42f17e61b3aed69d0
mshahrasbi <mshahrasbi@msn.com>
2020-01-06 14:27:37 -0700

    adding friendship

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'my_protfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
 
   resources :user_stocks, only: [:create, :destroy]
 end
