ede75eb716e52a90ecb478af5ac7bd046016a686
mshahrasbi <mshahrasbi@msn.com>
2020-01-15 15:12:05 -0700

    search methods for looking up tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
     end
 
     def search
-
+        puts params[:search_param]
+        @users = User.search(params[:search_param])
+        render json: @users
     end
 
 end
